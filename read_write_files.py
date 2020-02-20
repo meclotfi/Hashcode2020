6fa5c677587209cbecbeefb5dcecdce8dd52a2df
meclotfi <40919923+meclotfi@users.noreply.github.com>
2020-02-20 20:39:23 +0100

    Update read_write_files.py

--- read_write_files.py
+++ read_write_files.py
@@ -10,17 +10,45 @@ days_scan = int(line1[2])
 books_scores = [int(x) for x in F.readline().split()]
 # libraies and thier content
 Libraries = {}  # libraries information
+# initialization of libraries
+
+
+def sum_score(l):
+    sum = 0
+    for x in l:
+        sum = sum+books_scores[x]
+    return sum
+
+
 i = 0
 t = F.readline().split()
+Max_score_lib = 0
+Books_per_day_Max = 0
 while (t != []):
 
     l1 = [int(x) for x in t]
     print(l1)
     t = F.readline().split()
     books_in_lib = [int(x) for x in t]
-    Libraries[i] = [l1[0], l1[1], l1[2], books_in_lib]
+    # calculer somme de score pour chaque library
+    score = sum_score(books_in_lib)
+    if(score > Max_score_lib):
+        Max_score_lib = score
+    if(Books_per_day_Max < l1[2]):
+        Books_per_day_Max = l1[2]
+
+    Libraries[i] = [l1[0], l1[1], l1[2], score, books_in_lib]
     i = i+1
     t = F.readline().split()
+
+print(Books_per_day_Max)
+
+
+def score_normalization():
+    for lib in Libraries.items:
+        lib[3] = lib[3]/Max_score_lib
+
+
 print(Libraries)
 # traitement ici
 
